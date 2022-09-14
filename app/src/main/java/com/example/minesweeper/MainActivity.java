63cee428896aa516b3fe4feac545a8d412a8f910
Naomi Ramos Benitez <ramosben@usc.edu>
2022-09-14 16:04:19 -0700

    created a structure for the game

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -1,14 +1,114 @@
 package com.example.minesweeper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private int clock = 0;
+    private boolean running = false;
+    private static final int COLUMN_COUNT = 8;
+
+    // save the TextViews of all cells in an array, so later on,
+    // when a TextView is clicked, we know which cell it is
+    private ArrayList<TextView> cell_tvs;
+
+    private int dpToPixel(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cell_tvs = new ArrayList<TextView>();
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
+        // Method (3): add four dynamically created cells with LayoutInflater
+        LayoutInflater li = LayoutInflater.from(this);
+        for (int i = 3; i<=12; i++) {
+            for (int j=0; j<=7; j++) {
+                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
+                tv.setBackgroundColor(Color.GREEN);
+                tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+
+                cell_tvs.add(tv);
+            }
+        }
+
+        if (savedInstanceState != null) {
+            clock = savedInstanceState.getInt("clock");
+            running = savedInstanceState.getBoolean("running");
+        }
+
+        runTimer();
+    }
+
+    private int findIndexOfCellTextView(TextView tv) {
+        for (int n=0; n<cell_tvs.size(); n++) {
+            if (cell_tvs.get(n) == tv)
+                return n;
+        }
+        return -1;
+    }
+
+    public void onClickTV(View view) {
+        TextView tv = (TextView) view;
+        int n = findIndexOfCellTextView(tv);
+        int i = n / COLUMN_COUNT;
+        int j = n % COLUMN_COUNT;
+        tv.setText(String.valueOf(i) + String.valueOf(j));
+        if (tv.getCurrentTextColor() == Color.GRAY) {
+            tv.setTextColor(Color.GREEN);
+            tv.setBackgroundColor(Color.parseColor("lime"));
+        } else {
+            tv.setTextColor(Color.GRAY);
+            tv.setBackgroundColor(Color.LTGRAY);
+        }
+        onClickStart(view);
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("clock", clock);
+        savedInstanceState.putBoolean("running", running);
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.textView11);
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int seconds = clock;
+                String time = String.format("%d", seconds);
+                timeView.setText(time);
+
+                if (running) {
+                    clock++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
